36f349dba4426a86881e2d731973819ca5ca662e
Sahil Arora <sahila@kpit.com>
2019-03-11 18:10:17 +0530

    Child Routes and Routing Module Added

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -11,12 +11,25 @@ import { ActivatedRoute } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService,private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService,private route:ActivatedRoute,
+    private router:Router
+    ) { }
 
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     console.log(id);
     this.server = this.serversService.getServer(id);
+
+    this.route.params.subscribe((params)=>{
+      const id = +params['id'];
+      console.log(id);
+      this.server = this.serversService.getServer(id);
+  
+    })
+  }
+  onEdit(){
+    // this.router.navigate starts from starting
+this.router.navigate(['edit'],{relativeTo:this.route,queryParamsHandling:'preserve'})
   }
 
 }
